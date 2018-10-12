23f0a9832638b8106b285813e12301e990b06016
Barry Hunter <github@barryhunter.co.uk>
2018-10-12 12:32:12 +0100

    first initial version

---
+++ Leaflet.GeographPhotos.js
@@ -0,0 +1,372 @@
+/**
+ * GeoGraph geographic photo archive project
+ * This file copyright (C) 2018  Barry Hunter (geo@barryhunter.co.uk)
+ *
+
+The MIT License (MIT)
+
+Copyright (c) 2018  Barry Hunter
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+ */
+
+/* Plots Geograph Photos on a Leaflet Map!
+*
+* Prerequisites:
+*   https://github.com/ded/reqwest
+*   https://github.com/turban/Leaflet.Photo/
+*      https://github.com/Leaflet/Leaflet.markercluster/
+*   https://github.com/domoritz/leaflet-maskcanvas/
+*      uses reworked copy of https://github.com/jsmarkus/ExamplesByMesh/tree/master/JavaScript/QuadTree 
+**/  
+
+
+if (L.MarkerClusterGroup && L.Photo.Cluster) {
+
+	L.GeographPhotos = L.Photo.Cluster.extend({
+		options: {
+			//MarkerClusterGroup options, NOTE: PLEASE don't make maxClusterRadius than 60, as loads lots of thumbnails!
+			maxClusterRadius:80, showCoverageOnHover: true, spiderfyDistanceMultiplier: 2,
+
+			//geographPhotos options, shouldnt need to change these, except the api_key
+			apiKey: 'geograph_demo', //get your own key: https://www.geograph.org.uk/admin/apikey.php
+			endPoint: 'https://api.geograph.org.uk/api-facetql.php',
+			fieldSelect: "id,realname,wgs84_lat+as+lat,wgs84_long+as+lng,hash,title", //shouldnt need changing!
+			initialLimit: 500,
+			refreshLimit: 100,
+
+			showPhotoLayer: true, //can turn off the images, but that would be unusual!
+			showDotsLayer: false, //if turn this on, need to load leaflet-maskcanvas first
+			autoZoomOnAdd: false,
+
+			//can filter the layer
+			searchQuery: '',
+			userID: null, //todo, not implemented yet!
+
+			//general. Note: can narrow the bounds, but please dont make wider, we dont have photos outside these bounds!
+			bounds: L.latLngBounds(L.latLng(49.863788, -13.688451), L.latLng(60.860395, 1.795260)), 
+			minZoom: 4, maxZoom: 21
+		},
+
+		initialize: function (options) {
+			L.setOptions(this, options);
+			L.Photo.Cluster.prototype.initialize.call(this);
+
+			if (this.options.showDotsLayer && L.TileLayer.maskCanvas) {
+				this._masklayer = L.TileLayer.maskCanvas({noMask:true, radius: 2, useAbsoluteRadius: false });
+				this._layerData = new Array();
+			}
+
+			//simple associative array to avoid adding the same thumbnail to photo data
+			/// ... note we just keep adding data to photoLayer as zoom, we dont remove the out of view data (no point!) 
+			this._done = new Array();
+
+			if (!this.options.showPhotoLayer)
+				this.options.fieldSelect = "wgs84_lat+as+lat,wgs84_long+as+lng";
+
+			this.on('click', function (evt) {
+		                var photo = evt.layer.photo,
+		                        template = '<a href="{link}" target=newwin><img src="{url}"/></a><p>{caption}</p>';
+
+		                evt.layer.bindPopup(L.Util.template(template, photo), {
+		                        className: 'leaflet-popup-photo',
+		                        minWidth: 300
+		                }).openPopup();
+		        });
+
+			this._request = null; //the currently running reqwest
+
+			//is the a fetch in progress?
+			this._running = false;
+
+			//these are for zoomin optimization (if prev zoom had all markers then no need to load them again for zooming in)
+			this._prevZoom = -1;
+			this._shownall = false;
+			this._sentBounds = '';
+			this._prevPoint = null;
+			this._totalImages = null;
+		},
+
+	///////////////////////////////////////////////
+
+	        onAdd: function (map) {
+                    L.Photo.Cluster.prototype.onAdd.call(this,map);
+	            this._map = map;
+
+		    if (this._totalImages) { //there are images to show (initialRequest already called)
+			map.on('moveend', this.requestData, this);
+	                this.requestData();
+		    } else if (!this._initialDone) { //just in case it was called, but found no results
+  	                this.initialRequest(); //will add moveend if needed!
+
+			//we already zoomed in, then should make a detailed request too
+			if (!this.options.autoZoomOnAdd && map.getZoom() > 8) {
+				var that = this;
+				setTimeout(function () { that.requestData.call(that); } , 600);
+			}
+                    }
+
+		    if (this._masklayer)
+			map.addLayer(this._masklayer);
+	        },
+
+	        /**
+	            Remove the 'moveend' event listener and clear all the markers.
+        	    @private
+	        */
+	        onRemove: function (map) {
+                    L.Photo.Cluster.prototype.onRemove.call(this,map);
+
+	            map.off('moveend', this.requestData, this);
+	            this.clear();
+		    this._done = new Array(); //need to clear these so the layer will work if/when re-added
+		    this._shownall = false;
+   		    this._prevPoint = null;
+ 
+                    if (this._masklayer) {
+                        this._layerData = new Array();
+			this._masklayer.setData(this._layerData);
+			map.removeLayer(this._masklayer);
+                    }
+
+		    this.outputStatus('');
+	        },
+
+
+	        /**
+	            Redraws the data, clear current state, and starts a fresh initial request. Call this if change .options.searchQuery and/or options.userID
+	            @public
+	        */
+       		Reset: function () {
+			this.clear();
+                        this._done = new Array(); //need to clear these so the layer will work if/when re-added
+                        this._shownall = false;
+                        this._prevPoint = null;
+
+                        if (this._masklayer) {
+                            this._layerData = new Array();
+                            this._masklayer.setData(this._layerData);
+                        }
+
+	                this.initialRequest();
+
+                        //we already zoomed in, then should make a detailed request too
+                        if (!this.options.autoZoomOnAdd && this._map.getZoom() > 8) {
+                                var that = this;
+                                setTimeout(function () { that.requestData.call(that); } , 600);
+                        }
+	        },
+
+
+		outputStatus: function (text) {
+			if (document.getElementById('message')) //todo make this optional!
+				document.getElementById('message').innerHTML = text;
+		},
+
+	///////////////////////////////////////////////
+
+		initialRequest: function() {
+
+			//make the initial request, that calls fitBounds to zoom map to extent of query results. The 'order=sequence' is magic in that results should be relatively evenly distributed over the whole map
+			query = encodeURIComponent(this.options.searchQuery+(this.options.userID?' @user user'+this.options.userID:''));
+
+			var that = this; //enclosure!
+			this._request = reqwest({
+				url: this.options.endPoint+'?match='+query+'&select='+this.options.fieldSelect+'&order=sequence+asc&limit='+this.options.initialLimit+'&callback=bar&key='+this.options.apiKey,
+				type: 'jsonp',
+				jsonpCallbackName: 'bar', //we use a specific callback name, so it has consistent name, and hence the server can cache this API call. 
+				success: function (data) {
+					if (!data.rows || data.meta.total ==0) {
+						alert("no results found");
+						return;
+					}
+
+					that.outputStatus("Loading "+data.rows.length+" images");
+
+					//add results to the map
+					that.addRows(data.rows);
+
+					if (that.options.autoZoomOnAdd && that._map.hasLayer(that))
+						that._map.fitBounds(that.getBounds());
+
+					if (data.meta.total_found > data.rows.length) {
+						var message = data.rows.length+' of '+data.meta.total_found+' images';
+						that._totalImages = data.meta.total_found;
+
+						//there are more results than displayed, so update the map when zoom
+						that._map.on('moveend', that.requestData, that);
+					} else {
+						var message = data.meta.total_found+' images';
+
+						//we've displayed all matching images, so dont need to update fetch more results when zoom
+					}
+
+					that.outputStatus("Loaded "+message);
+					this._initialDone = true;
+				}
+			});
+		},
+
+	///////////////////////////////////////////////
+
+	        /**
+	            Send a query request for JSONP data.
+	            @private
+	        */
+	        requestData: function () {
+			var map = this._map;
+
+	                //if ((!this._masklayer || !map.hasLayer(this._masklayer)) && !map.hasLayer(this)) //shouldn't happen, but just in case!
+        	        //        return;
+
+			var center = map.getCenter();
+			var point1 = map.getPixelOrigin();
+			if (this._prevPoint && map.getZoom() == this._prevZoom) {
+				if (Math.abs(point1.x-this._prevPoint.x) <10 && Math.abs(point1.y-this._prevPoint.y) <10) {
+					return false;
+				}
+			}
+			this._prevPoint = point1;
+
+			if (this._running) {
+				if (this._request) this._request.abort();
+				this._running = false;
+			}
+
+			if (this._shownall == false || map.getZoom() <= this._prevZoom) {
+				var bounds = map.getBounds();
+				var zoom = map.getZoom();
+				this._sentBounds = bounds.toBBoxString();
+
+				this.outputStatus("Making request...")
+				this._running = true;
+
+				var query = encodeURIComponent(this.options.searchQuery+(this.options.userID?' @user user'+this.options.userID:''));
+				var that = this; //enclosure!
+				this._request =  reqwest({
+	                		url: this.options.endPoint+'?match='+query+'&olbounds='+this._sentBounds+'&select='+this.options.fieldSelect+'&order=rand()&limit='+this.options.refreshLimit+'&callback=?&key='+this.options.apiKey,
+			                type: 'jsonp',
+			                success: function (data) {
+						that._running = false;
+		        	                if (!data.rows || data.meta.total ==0) {
+		                          	      return;
+			                        }
+						that.outputStatus("Loading "+data.rows.length+" images");
+
+						//add the results to map
+			                        that.addRows(data.rows);
+
+			                        if (data.meta.total_found > data.rows.length) {
+							that.outputStatus("Loaded "+data.rows.length+" of "+data.meta.total_found+" images in current view");
+							that._shownall = false;
+			                        } else {
+							that.outputStatus("Loaded all "+data.rows.length+" images in current view");
+							that._shownall = true;
+			                        }
+	               		 	}
+			        });
+			}
+			this._prevZoom = map.getZoom();
+		},
+
+	///////////////////////////////////////////////
+
+ 		addRows: function(rows) {
+			var newRows = new Array();
+
+		        for(q=0;q<rows.length;q++) {
+				if (!this._done[rows[q].id]) {
+					//add the thumbnail to the clusterer data
+                		        row = rows[q];
+		                        row.link = "https://www.geograph.org.uk/photo/"+row.id; //todo make this use an option!
+                		        row.thumbnail = this.getGeographUrl(row.id, row.hash, 'small');
+		                        row.url = this.getGeographUrl(row.id, row.hash, 'full');
+                		        row.caption = row.title+' by '+row.realname;
+		                        row.lat = this.rad2deg(row.lat);
+		                        row.lng = this.rad2deg(row.lng);
+					newRows.push(row);
+					this._done[row.id] = 1;
+
+					//just add the location to the dots layer
+					if (this._masklayer)
+						this._layerData.push([row.lat,row.lng]);
+				}
+		        }
+
+			//add the new photos
+			if (this.options.showPhotoLayer) //we are the photo layer, so it can't be not added
+			        this.add(newRows);
+
+			//update the dots layer
+			if (this._masklayer) {
+				this._masklayer.setData(this._layerData);
+			}
+		},
+
+	///////////////////////////////////////////////
+
+		rad2deg: function(angle) {
+		    // Converts the radian number to the equivalent number in degrees  
+		    // 
+		    // version: 1109.2015
+		    // discuss at: http://phpjs.org/functions/rad2deg
+		    // +   original by: Enrique Gonzalez
+		    // +      improved by: Brett Zamir (http://brett-zamir.me)
+		    // *     example 1: rad2deg(3.141592653589793);
+		    // *     returns 1: 180
+		    return angle * 57.29577951308232; // angle / Math.PI * 180
+		},
+
+		getGeographUrl: function(gridimage_id, hash, size) { 
+
+			yz=this.zeroFill(Math.floor(gridimage_id/1000000),2); 
+			ab=this.zeroFill(Math.floor((gridimage_id%1000000)/10000),2); 
+			cd=this.zeroFill(Math.floor((gridimage_id%10000)/100),2);
+			abcdef=this.zeroFill(gridimage_id,6); 
+
+			if (yz == '00') {
+				fullpath="/photos/"+ab+"/"+cd+"/"+abcdef+"_"+hash; 
+			} else {
+				fullpath="/geophotos/"+yz+"/"+ab+"/"+cd+"/"+abcdef+"_"+hash; 
+			}
+	
+			switch(size) { 
+				case 'full': return "https://s0.geograph.org.uk"+fullpath+".jpg"; break; 
+				case 'med': return "https://s"+(gridimage_id%4)+".geograph.org.uk"+fullpath+"_213x160.jpg"; break; 
+				case 'small': 
+				default: return "https://s"+(gridimage_id%4)+".geograph.org.uk"+fullpath+"_120x120.jpg"; 
+			}
+		},
+
+		zeroFill: function(number, width) {
+			width -= number.toString().length;
+			if (width > 0) {
+				return new Array(width + (/\./.test(number)?2:1)).join('0') + number;
+			}
+			return number + "";
+		}
+
+	});
+
+	L.geographPhotos = function (options) {
+		return new L.GeographPhotos(options);	
+	};
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,64 @@
 # Leaflet.GeographPhotos
 Plots Geograph Photos on a Leaflet Map!
+
+Extends the most excellent (and very tiny!) Leaflet.Photo plugin,
+http://blog.mastermaps.com/2014/08/showing-geotagged-photos-on-leaflet-map.html
+ but intergrates calling the Geograph API to load photos. Including dynamically loading more as zoom the map. 
+
+Please contact Geograph before using, you also need a API key
+https://www.geograph.org.uk/help/api
+
+At the moment, hard coded to work with Geograph Britain and Ireland, with minor adaptations should be able to work with other projects. 
+
+
+Note: This plugin is heavily inspired by the demos from Leaflet.Photo, and as such uses its image popup styling. 
+as such have moved the leaflet-popup-photo CSS class from Leaflet.Photo/examples/css/map.css, to Leaflet.Photo/Leaflet.Photo.css, 
+so this project can just import one .css file
+
+## Demo
+https://www.geograph.org/leaflet/GeographPhotos-example.html
+
+
+## Prerequisites:
+*   https://github.com/Leaflet/Leaflet/    (obviouslly!)
+*   https://github.com/turban/Leaflet.Photo/    (a copy is bunded)
+*   https://github.com/ded/reqwest/              (the copy that Leaflet.Photo used is bunded) 
+*   https://github.com/Leaflet/Leaflet.markercluster/   (copy loaded from CDN in example) 
+
+## Optional: (can also display a layer of dots highlighting where photos are located)
+*   https://github.com/domoritz/leaflet-maskcanvas/
+*      uses reworked copy of https://github.com/jsmarkus/ExamplesByMesh/tree/master/JavaScript/QuadTree
+
+## Clearly inspired by and draws ideas from:
+*   https://github.com/bill-chadwick/Leaflet.MetricGrid/
+*   https://github.com/MatthewBarker/leaflet-wikipedia/
+*   https://github.com/turban/Leaflet.Photo/
+
+## See Also 
+*   https://leafletjs.com/examples/quick-start/
+*   https://github.com/bill-chadwick/Leaflet.MetricGrid/    (Display a dynamic OSGB and Irish Grid on map) 
+*   https://github.com/barryhunter/Leaflet.GeographCoverage/    (Displays a coverage aggrigated by squares)
+
+
+## Options
+
+* **apiKey**: Get your own API key! https://www.geograph.org.uk/admin/apikey.php
+* **endPoint**: The Geograph API endpoint to use, shouldnt need changing
+* **fieldSelect**: The values to pull from API, shouldnt need to change, unless make a more elaborate popupl and want more detail
+* **initialLimit**: default 500. How many photos to load initially. All will show as dots, but due to clustering, usually many less thumbnails show
+* **refreshLimit**: default 100. How many photos to load each time zoom/move the map
+
+
+* **showPhotoLayer**: default true, but can disable the Photo layer (but will stil need to load Leaflet.Photo even though its 'unused'
+* **showDotsLayer**: default false. can enable to show a preview of dots coverage. leaflet-maskcanvas is only needed if enabled
+* **autoZoomOnAdd**: default false. should the map be zoomed to extent of photos when first loaded (or query is changed) 
+
+* **searchQuery**: Full-text query to filter imagses. Same syntax as https://www.geograph.org.uk/article/Keyword-Searching-in-the-Browser
+* **userID**: Can optionally filter by a Geograph User/Contributor ID.  
+
+as extends BOTH Leaflet.Photo and Leaflet.markercluster, most options from those plugins can be used too. For example: 
+
+* **showCoverageOnHover**: default true. When you mouse over a cluster it shows the bounds of its markers.
+
+NOTE: PLEASE don't make maxClusterRadius less than 60, as loads lots of thumbnails!
+
